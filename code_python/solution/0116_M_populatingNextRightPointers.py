83765a805ae171959e7fcbbd4d44afd49676c299
RunAtWorld <hudalpf@163.com>
2022-01-17 01:34:41 +0800

    116. 填充每个节点的下一个右侧节点指针

--- code_python/solution/0116_M_populatingNextRightPointers.py
+++ code_python/solution/0116_M_populatingNextRightPointers.py
@@ -3,6 +3,9 @@
 
 """
 116. 填充每个节点的下一个右侧节点指针
+a. BFS
+b. BFS-DFS
+c. 使用已建立的 next 指针.
 @Time : 2022/1/16 23:37
 @Author: RunAtWorld
 @File: 0116_M_populatingNextRightPointers.py
@@ -22,6 +25,31 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        """
+        方法：使用已建立的 next 指针.
+        (0)每次将指针指向这一层最左边的位置
+        (1) 连接1: 连接同一个父节点的两个子节点。它们可以通过同一个节点直接访问到。
+        (2) 连接2: 在不同父亲的子节点之间建立连接: 第 N 层节点之间建立 next 指针后，再建立第 N+1 层节点的 next 指针。
+        """
+        if not root:
+            return root
+        # leftmost 是最左边的节点,从根节点开始
+        leftmost = root
+        while leftmost.left:
+            # 遍历上面一层节点组织成的链表，为下一层的节点更新 next 指针
+            up_head = leftmost
+            while up_head:
+                # 连接1: 连接同一个父节点的两个子节点
+                up_head.left.next = up_head.right
+                # 连接2: 在不同父亲的子节点之间建立连接
+                if up_head.next:
+                    up_head.right.next = up_head.next.left
+                # 指针向后移动
+                up_head = up_head.next
+            leftmost = leftmost.left
+        return root
+
+    def connect2(self, root: 'Optional[Node]') -> 'Optional[Node]':
         """
         BFS: 遍历时记录每一层节点的个数。
         BFS遍历时，出队不一定只是出一次！！！
